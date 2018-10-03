ea29a54b61796242aaa024c8e05f2d89cc028902
Nicholas Olds <oldsn@Nicholass-MacBook-Pro.local>
2018-10-03 16:15:35 -0700

    added functionality for floating point numbers with method, calculateDouble

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -51,6 +51,49 @@ public class Calculator {
         return ret
     }
     
+    // For Floating Point Numbers *EC*
+    public func calculateDouble(_ args: [String]) -> Double {
+        var ret : Double = 0.0
+        if args.contains("count") { // count of numbers
+            ret = Double(args.count) - 1.0
+        } else if args.contains("avg") { // average of numbers
+            for i in args {
+                let temp : Double = Double(i) ?? 0.0
+                ret = ret + temp
+            }
+            if ret != 0 {
+                ret = ret / (Double(args.count) - 1)
+            }
+        } else if args.contains("fact") { // factorial
+            if args.count == 2 {
+                ret = 1.0
+                let upper : Int = Int(args[0]) ?? 0
+                for i in 1..<(upper + 1){
+                    ret = ret * Double(i)
+                }
+            }
+        } else { // traditional calculations
+            let left : Double = Double(args[0]) ?? 0.0
+            let op = args[1]
+            let right : Double = Double(args[2]) ?? 0.0
+            switch op {
+            case "+":
+                ret = left + right
+            case "-":
+                ret = left - right
+            case "/":
+                ret = left / right
+            case "*":
+                ret = left * right
+            case "%":
+                ret = left.truncatingRemainder(dividingBy: right)
+            default:
+                print("Operator not valid")
+            }
+        }
+        return ret
+    }
+    
     // makes String input into [String]
     public func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
@@ -71,5 +114,4 @@ if CommandLine.argc > 1 {
     let second = readLine()!
     print(Calculator().calculate([first, operation, second]))
 }
-
 
